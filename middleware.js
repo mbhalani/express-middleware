9678c674aee26510c43d3187edbab1fc2dd018e8
Mukesh B <mbhalani3@gmail.com>
2017-03-09 14:47:56 -0800

    first commit

---
+++ middleware.js
@@ -0,0 +1,19 @@
+// express server
+const express = require('express');
+const port = process.env.PORT || 4008;
+const ip = process.env.IP || '127.0.0.1';
+
+const app = express();
+
+// ========== Middleware ========== //
+
+
+// ========== END Middleware ========== //
+
+app.get('/', (request, response) => {
+  response.send('Hello world!');
+});
+
+app.listen(port, ip, () => {
+  console.log('Server listening on ip:port - ' + ip + ':' + port);
+});
